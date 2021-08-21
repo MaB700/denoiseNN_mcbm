31b7f82a77e7876a40bc5315c4d164c2d020ca05
Schochx <schoch1995@gmail.com>
2021-08-21 23:05:57 +0200

    update

--- denoiseNN_mcbm.py
+++ denoiseNN_mcbm.py
@@ -1,10 +1,12 @@
 # %%
+#load modules
 from __future__ import absolute_import, division, print_function, unicode_literals
+import os
 import pandas as pd
 import numpy as np
 import ipywidgets as widgets
 from ipywidgets import fixed
-
+#os.environ["CUDA_VISIBLE_DEVICES"]="-1" #uncomment to use cpu
 import tensorflow as tf
 #os.environ['AUTOGRAPH_VERBOSITY'] = 1
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -15,22 +17,22 @@ if gpus:
 
     except RuntimeError as e:
         print(e)
-from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose
+from tensorflow.keras.layers import InputLayer, Conv2D, Conv2DTranspose
 from tensorflow.keras.models import Sequential
 
 # load custom functions/loss/metrics
 from denoiseNN_functions import *
 
-import wandb
-from wandb.keras import WandbCallback
+#import wandb
+#from wandb.keras import WandbCallback
 #wandb.init(project="autoencoder_mcbm_toy_denoise")
 
 print('Tensorflow version: ' + tf.__version__)
 print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT available")
 # %%
-# load dataset
-# header gives information of parameters
-# TODO: load parameters from file header
+#load dataset
+#header gives information of parameters
+#TODO: load parameters from file header
 nofEvents_train = 20000
 nofEvents_test = 3213
 cut_range = 20.0
@@ -107,13 +109,11 @@ test2 = tf.clip_by_value(test2, clip_value_min=0., clip_value_max=1.)
 #print(test2)
 #single_event_plot(test,test,px_x, -8.1, 13.1, px_y, -23.85, 23.85, 7, cut =0 )
 
-""" interactive_plot = widgets.interact(single_event_plot, \
-                    data=fixed(test2), data0=fixed(test), \
-                    nof_pixel_X=fixed(px_x), min_X=fixed(-13.1), max_X=fixed(8.1), \
-                    nof_pixel_Y=fixed(px_y), min_Y=fixed(-23.85), max_Y=fixed(23.85), eventNo=(0,len(hits_all_test[:,0])-1,1), cut=(0.,0.90,0.05))
- """
-# %%
+#interactive_plot = widgets.interact(single_event_plot, data=fixed(test2), data0=fixed(test), nof_pixel_X=fixed(px_x), min_X=fixed(-13.1), max_X=fixed(8.1), \
+                    #nof_pixel_Y=fixed(px_y), min_Y=fixed(-23.85), max_Y=fixed(23.85), eventNo=(0,len(hits_all_test[:,0])-1,1), cut=(0.,0.90,0.05))
 
+# %%
+#preprocessing
 hits_all_train = tf.cast(train[..., tf.newaxis],dtype=tf.float32)
 hits_all_test = tf.cast(test[..., tf.newaxis],dtype=tf.float32)
 hits_true_train = tf.cast(train2[..., tf.newaxis],dtype=tf.float32)
@@ -140,11 +140,12 @@ hits_test = tf.concat([hits_true_test, noise_test, order1_test, order2_test], 3)
 #del noise_train, noise_test, order1_train, order2_train, order1_test, order2_test #free up momory
 
 # %%
+#train nn
 custom_metrics = [get_hit_average(), get_noise_average(), get_background_average(),\
                     get_hit_average_order1(), get_hit_average_order2()]
 
 model = Sequential()
-model.add(Input(shape=(72, 32, 1)))
+model.add(InputLayer(input_shape=(72, 32, 1)))
 
 model.add(Conv2D(filters=32, kernel_size=3, strides=2,activation='relu', padding='same'))
 model.add(Conv2D(filters=64, kernel_size=3, strides=[2, 2],activation='relu', padding='same'))
@@ -158,10 +159,10 @@ model.add(Conv2D(1, kernel_size=3, activation='tanh', padding='same'))
 model.summary()
 #opt = tf.keras.optimizers.Adadelta(lr=0.1, rho=0.95, epsilon=1e-07 )
 opt = tf.keras.optimizers.Adam(learning_rate=0.001)
-model.compile(optimizer=opt, loss=get_custom_loss(), metrics=custom_metrics, experimental_steps_per_execution=10)
+model.compile(optimizer=opt, loss=get_custom_loss(), metrics=custom_metrics)
 es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='min')
 model.fit(hits_all_train, hits_train,
-                epochs=50,
+                epochs=5,
                 batch_size=300,
                 shuffle=True,
                 validation_data=(hits_all_test, hits_test),
@@ -201,4 +202,3 @@ interactive_plot = widgets.interact(single_event_plot, \
 # #print(hits_test[0,:,:,2])
 # # %%
 
-# %%
